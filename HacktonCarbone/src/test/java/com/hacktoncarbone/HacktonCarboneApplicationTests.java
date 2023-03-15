b2efb7bbe8c2ccb1d72630ddd0de20b374b4e4d7
Corentin <sh4d0w.st@gmail.com>
2023-03-15 10:35:52 +0100

    initialisation module spring

---
+++ HacktonCarbone/src/test/java/com/hacktoncarbone/HacktonCarboneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hacktoncarbone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HacktonCarboneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
