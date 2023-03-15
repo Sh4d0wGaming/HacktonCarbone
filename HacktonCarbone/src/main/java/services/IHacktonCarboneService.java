e6ddbfaed027d1a0ff074b22c60989be75c832fe
Sh4d0wGaming <sh4d0w.st@gmail.com>
2023-03-15 16:44:38 +0100

    Delete HacktonCarbone directory

--- HacktonCarbone/src/main/java/services/IHacktonCarboneService.java
+++
@@ -1,9 +0,0 @@
-package services;
-
-import models.Habitation;
-import models.Vehicule;
-
-public interface IHacktonCarboneService {
-    public double CalculateHabitation(Habitation habitation);
-    public double CalculateVehicule(Vehicule vehicule);
-}
